e8a4aeb6b5cbbd25e5111e227b49716ab7da2064
Roy Kreiner <roy.kreiner@mail.huji.ac.il>
2022-11-23 18:35:01 +0200

    added files class with attributes

---
+++ src/Directories.java
@@ -0,0 +1,2 @@
+public class Directories {
+}
--- src/Files.java
+++ src/Files.java
@@ -1,2 +1,34 @@
+import java.util.Date;
+
+/*  A File class has the following attributes
+● name - up to 32 characters long
+● size - positive long integer
+● creation date (date type)
+*/
+
 public class Files {
+
+    // private attributes
+    private String name;
+    private long size;
+    private Date creationDate;
+
+    // public attributes
+
+
+    // Constructor with 3 params (name, size, creation date)
+    public Files(String name, long size, Date creationDate) {
+
+        // check name is longer then 32 characters
+        if (name.length() > 32) {
+            System.out.println("Name is too long");
+        } else {
+            this.name = name;
+        }
+
+        this.name = name;
+        this.size = size;
+        this.creationDate = creationDate;
+    }
+
 }
